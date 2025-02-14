9e174daa3e7751cdd786a23fd153be7ab5ee444a
RAJI-Zakaria <zakariaraji.me@gmail.com>
2025-02-14 16:27:31 +0100

    setting the classnames for the tailwindcss

--- app/index.tsx
+++ app/index.tsx
@@ -5,7 +5,7 @@ import "../global.css";
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center">
-      <Text className="text-9xl">Airakaz - React is awesome22</Text>
+      <Text className="text-2xl">Airakaz - React is awesome22</Text>
       <StatusBar style="auto" />
       <Link href="/profile">Go to Profile</Link>
     </View>

8b9836cd3d1e4913ebfc7b3e8455035815de6d23
RAJI-Zakaria <zakariaraji.me@gmail.com>
2025-02-14 16:16:56 +0100

    setting up nativewind

--- app/index.tsx
+++ app/index.tsx
@@ -1,22 +1,13 @@
 import { Link } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-
+import "../global.css";
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Airakaz - React is awesome22</Text>
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-9xl">Airakaz - React is awesome22</Text>
       <StatusBar style="auto" />
       <Link href="/profile">Go to Profile</Link>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
